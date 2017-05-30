12d77229aacd2c06d8e7ee7e2f39d7d8c100b0bc
lindsim <lindsey.simard@gmail.com>
2017-05-30 13:29:44 -0700

    positioning corrected; arrow keys being read

--- tangrams/manipulation.js
+++ tangrams/manipulation.js
@@ -1,10 +1,36 @@
 $(function() {
-  $(".shape").draggable({
-    containment: "#playspace"
-  });
+
   $("#start").position({
     my: "center",
     at: "center",
     of: "#playspace"
+  });
+  
+  $(".shape").draggable({
+    containment: "#playspace"
+  });
+
+	$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: alert( xmouse );
+        break;
+
+        case 38: alert( "up" );
+        break;
+
+        case 39: alert( "right" );
+        break;
+
+        case 40: alert( "down" );
+        
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+	});
+
+  $(".shape").click(function(){
+  	$(this).toggleClass("turn");
   });
 });
