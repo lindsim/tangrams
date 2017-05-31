f3e064bdf77ae5dedc6823c1c7c4376dbf8aeda4
lindsim <lindsey.simard@gmail.com>
2017-05-31 13:30:27 -0700

    adding movement

--- tangrams/manipulation.js
+++ tangrams/manipulation.js
@@ -1,10 +1,36 @@
 $(function() {
-  $(".shape").draggable({
-    containment: "#playspace"
-  });
+
   $("#start").position({
     my: "center",
     at: "center",
     of: "#playspace"
   });
+  
+  $(".shape").draggable({
+    containment: "#playspace"
+  });
+  
+	var mouseoverTar = $(document).mouseover(function(){
+   target.attr("id")
+  })
+
+	$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: alert("left");
+        break;
+
+        case 38: alert("up");
+        break;
+
+        case 39: alert("right");
+        break;
+
+        case 40: alert("down");
+        
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+	});
 });
