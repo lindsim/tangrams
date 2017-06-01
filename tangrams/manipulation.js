f90fbccb82c47b35aed6002ecc74960020570ba0
lindsim <lindsey.simard@gmail.com>
2017-06-01 11:59:54 -0700

    added movement; need to make free play vs shape matching mode

--- tangrams/manipulation.js
+++ tangrams/manipulation.js
@@ -10,19 +10,24 @@ $(function() {
     containment: "#playspace"
   });
 
+  var currentShape = null;
+
+  $(".shape").mouseover(function() {
+    currentShape = $(this);
+  })
+
 	$(document).keydown(function(e) {
     switch(e.which) {
-        case 37: alert("left");
+        case 37: alert("pointleft");
         break;
 
-        case 38: alert("up");
+        case 38: alert("pointUp");
         break;
 
-        case 39: alert("right");
+        case 39: alert("pointRight");
         break;
 
-        case 40: alert("down");
-        
+        case 40: alert("pointDown");
         break;
 
         default: return; // exit this handler for other keys
