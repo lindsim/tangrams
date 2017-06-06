f8084410885e14cbe957a2f5a90e7f4258c8299e
lindsim <lindsey.simard@gmail.com>
2017-06-05 19:30:36 -0700

    moving square and triangles; time to make a mixin?

--- tangrams/manipulation.js
+++ tangrams/manipulation.js
@@ -1,31 +1,59 @@
 $(function() {
 
   $(".shape").draggable({
-    containment: "#playSpace"
+    containment: "#playspace"
   });
-
+  
   var currentShape = null;
 
-  $(".shape").click(function() {
-    currentShape = $(this);
+	$(".shape").mouseover(function() {
+  	currentShape = $(this);
   })
-
+  
+	var triangles = ["lgtri", "lgtri2", "smtri", "smtri2", "medtri"];
+  
 	$(document).keydown(function(e) {
-    switch(e.which) {
-        case 37: currentShape.toggleClass("pointLeft");
-        break;
+  	if ( currentShape.attr("id") === "lgtri") {//jquery.inArray($currentShape.attr("id"), triangles) != -1) {
+       switch(e.which) {
+        	case 37: 
+            currentShape.toggleClass("pointLeft");
+					  break;
 
-        case 38: currentShape.toggleClass("pointUp");
-        break;
+        	case 38: 
+            currentShape.toggleClass("pointUp");
+        	  break;
 
-        case 39: currentShape.toggleClass("pointRight");
-        break;
+        	case 39: 
+            currentShape.toggleClass("pointRight");
+       		  break;
+        
+        case 40: 
+            currentShape.toggleClass("pointDown");
+        	  break;
 
-        case 40: currentShape.toggleClass("pointDown");
-        break;
-
-        default: return; // exit this handler for other keys
+        	default: return; // exit this handler for other keys
+    	} 
+    } else if (currentShape.attr("id") === "square") {
+        switch(e.which) {
+          case 37:
+          case 39:
+            currentShape.toggleClass("turnRight");
+				    break;
+        
+          default: return; 
+   			}
+    } else {
+    	  switch(e.which) {
+         case 37:
+          console.log("parraaaalllellllooooooogrrrraaammm");
+          break;
+          
+        default: return; 
+        }
     }
-    e.preventDefault(); // prevent the default action (scroll / move caret)
-	});
+    e.preventDefault(); // prevent the default action from arrow keys
+  });
 });
+
+
+
