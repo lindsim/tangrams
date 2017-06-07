ba408774626a1709b4e74a2946a0215df9ff5196
lindsim <lindsey.simard@gmail.com>
2017-06-07 16:20:27 -0700

    movement is working but code needs to be refactored

--- tangrams/manipulation.js
+++ tangrams/manipulation.js
@@ -1,7 +1,7 @@
 $(function() {
 
   $(".shape").draggable({
-    containment: "#playspace"
+    containment: "#playspace", scroll: false
   });
   
   var currentShape = null;
@@ -11,43 +11,77 @@ $(function() {
   })
   
 	var triangles = ["lgtri", "lgtri2", "smtri", "smtri2", "medtri"];
-  
+  var triClasses = "rotate0 rotate45 rotate90 rotate135 rotate180 rotate225 rotate270 rotate315";
+  var parClasses = "parRight parLeft parUp parDown";
+
+  //this code needs to be refactored; there must be a better way to do this
+
 	$(document).keydown(function(e) {
   	if (triangles.indexOf(currentShape.attr("id")) >= 0 )  {
+
        switch(e.which) {
-        	case 37: 
-            currentShape.toggleClass("pointLeft");
+
+        	case 37:    
+            currentShape.removeClass(triClasses).addClass("rotate270")
 					  break;
 
         	case 38: 
-            currentShape.toggleClass("pointUp");
+            currentShape.removeClass(triClasses).addClass("rotate0")
         	  break;
 
-        	case 39: 
-            currentShape.toggleClass("pointRight");
+        	case 39:
+            currentShape.removeClass(triClasses).addClass("rotate90")
        		  break;
         
-        case 40: 
-            currentShape.toggleClass("pointDown");
+          case 40: 
+            currentShape.removeClass(triClasses).addClass("rotate180")
+        	  break;
+
+           case 191:
+            currentShape.removeClass(triClasses).addClass("rotate225")
+        	  break;
+           
+           case 13:
+            currentShape.removeClass(triClasses).addClass("rotate45")
+        	  break;
+           
+           case 16:
+            currentShape.removeClass(triClasses).addClass("rotate135")
+        	  break;
+           
+           case 222: 
+            currentShape.removeClass(triClasses).addClass("rotate315")
         	  break;
 
         	default: return; // exit this handler for other keys
     	}
-    } 
-   else if (currentShape.attr("id") === "square") {
+    } else if (currentShape.attr("id") === "square") {
         switch(e.which) {
           case 37:
           case 39:
-            currentShape.toggleClass("turnRight");
+            currentShape.toggleClass("rotate45");
 				    break;
+          
+          case 38:
+          case 40:
+            break;
         
           default: return; 
    			}
     } else {
     	  switch(e.which) {
          case 37:
-         case 39:
-          currentShape.toggleClass("transformRL")
+          currentShape.removeClass(parClasses).addClass("parLeft")
+          break;
+        case 39:
+          currentShape.removeClass(parClasses).addClass("parRight")
+          break; 
+        case 38:
+          currentShape.removeClass(parClasses).addClass("parUp")
+          break;
+
+        case 40: 
+          currentShape.removeClass(parClasses).addClass("parDown")
           break;
           
         default: return; 
