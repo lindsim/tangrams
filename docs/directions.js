d9e1f952de9cc5055bd1bfaff6332e2332cfd760
lindsim <lindsey.simard@gmail.com>
2017-08-08 20:03:40 -0700

    tried using linter again

--- docs/directions.js
+++ docs/directions.js
@@ -1,22 +1,22 @@
-$( function() {
+$(function () {
 
   var currentTemplate = $("#blank");
   var currentChoice = $("#0");
   var convert = ["blank", "one", "two", "three", "four", "five", "six"];
 
-  $(document).ready( function(){
+  $(document).ready(function (){
     $("#directText").hide();
     $(".templates").hide();
     currentTemplate.show();
   })
 
-  $("#directLink").hover( function(){
+  $("#directLink").hover(function (){
     $("#directText").show();
-  }, function(){
+  }, function (){
     $("#directText").hide();
   })
 
-  $(".choices").click( function() {
+  $(".choices").click(function () {
     currentTemplate.hide();
     currentChoice.removeClass("active");
     currentTemplate = $("#" + convert[($(this).attr("id")).toString()]);
