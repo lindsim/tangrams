fe7197da75c4187daa7ea5bb5a56516a16966fe7
lindsim <lindsey.simard@gmail.com>
2017-08-13 17:17:39 -0700

    fix problem with showing and hiding outline at start

--- docs/directions.js
+++ docs/directions.js
@@ -1,20 +1,20 @@
 $(function() {
-
-  var currentTemplate = $("#three");
-  var currentChoice = $("#3");
+  
+  var $currentOutline = $("zero");
+  var $currentChoice = $("#0");
+  var $outlines = $(".outlines");
+  var $choices = $(".choices");
+  
   var convert = ["blank", "one", "two", "three", "four", "five", "six"];
 
-  $(document).ready(function(){
-    $(".templates").hide();
-    currentTemplate.show();
-  })
+  $choices.click(function() {
+    $("three").removeClass("showing");
+    $currentOutline.removeClass("showing");
+    $currentOutline = $("#" + convert[($(this).attr("id")).toString()]);
+    $currentOutline.addClass("showing");
+    $currentChoice.removeClass("active");
+    $currentChoice = $(this);
+    $currentChoice.addClass("active");
 
-  $(".choices").click(function() {
-    currentTemplate.hide();
-    currentChoice.removeClass("active");
-    currentTemplate = $("#" + convert[($(this).attr("id")).toString()]);
-    currentChoice = $(this);
-    $(this).addClass("active");
-    currentTemplate.show();
-  })
+  });
 })
